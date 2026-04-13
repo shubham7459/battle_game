9a3bf9835d0f98c22db7abd3084f002d7c4de77e
shubham7459 <sy196335@gmail.com>
2026-04-13 17:22:51 +0530

    force fix allowed hosts

--- battle_game/settings.py
+++ battle_game/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
